b433c18cc0361cac5456cc4e03ba035b8ed3747a
kitihounel <kitihounel@gmail.com>
2021-08-31 16:46:53 +0100

    Worked on table component

--- src/app/doctors/doctor-list/doctor-list.component.ts
+++ src/app/doctors/doctor-list/doctor-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
 import { ModalService } from 'src/app/modal/modal.service'
 import { CrudTableViewConfig } from "src/app/shared/crud-table-view/crud-table-view.component"
+import { environment as env } from 'src/environments/environment'
+import { AuthService } from 'src/app/auth/auth.service'
 
 @Component({
   selector: 'app-doctor-list',
@@ -10,21 +14,39 @@ import { CrudTableViewConfig } from "src/app/shared/crud-table-view/crud-table-v
 export class DoctorListComponent implements OnInit {
 
   tableConfig: CrudTableViewConfig = {
-    indexColumnWidth: '5%',
-    actionsColumnWidth: '10%',
-    columnNames: ['Name', 'Speciality', 'Number', 'Contact'],
+    indexColumn: {
+      width: '5%'
+    },
+    actionsColumn: {
+      width: '10%'
+    },
+    columnNames:  ['Name', 'Speciality', 'Number', 'Contact'],
     columnWidths: ['30%', '20%', '20%', '15%'],
-    sortableColumns: new Set<number>(),
+    sortableColumns: new Set<number>([0, 1]),
     properties: ['name', 'speciality', 'number', 'contact'],
     data: [],
     showEmptyDatasetMessage: true
   }
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private http: HttpClient, private auth: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.get(`${env.apiUrl}/doctors`, {
+      'headers': {
+        'Authorization': `Bearer ${this.auth.user!.token}`
+      }
+    }).subscribe(
+      (obj: any) => {
+        this.tableConfig = { ...this.tableConfig, data: obj.data }
+      },
+      () => {
+        const dialog = this.modalService.createInfoDialog('Error', ['Unable to retrieve doctors from server.'])
+        dialog.afterClose().subscribe(() => this.tableConfig.data = [])
+        dialog.open()
+      }
+    )
+  }
 
   onPageChange($event: any) {
-    console.log($event)
   } 
 }
--- src/app/shared/crud-table-view/crud-table-view.component.ts
+++ src/app/shared/crud-table-view/crud-table-view.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core'
 
 export interface CrudTableViewConfig {
-  indexColumnWidth: string
-  actionsColumnWidth: string
-  columnNames: string[]
+  indexColumn?: {
+    width: string
+  }
+  actionsColumn?: {
+    width: string
+  }
+  columnNames:  string[]
   columnWidths: string[]
-  properties: string[]
+  properties:   string[]
   sortableColumns: Set<number>
   data: any[]
   showEmptyDatasetMessage?: boolean
@@ -16,12 +20,30 @@ export interface CrudTableViewConfig {
   templateUrl: './crud-table-view.component.html',
   styleUrls: ['./crud-table-view.component.css']
 })
-export class CrudTableViewComponent implements OnInit {
+export class CrudTableViewComponent implements OnInit, OnChanges {
 
   @Input() config!: CrudTableViewConfig
 
+  sortOrder = [] as number[]
+
   constructor() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    const data = changes.config.currentValue.data as any[]
+    this.sortOrder = data.map((v, i) => i)
+  }
+
   ngOnInit(): void {
+    this.sortOrder = this.config.data.map((v, i) => i)
+  }
+
+  sortByProperty(prop: string) {
+    const a = this.config.data.map((item, i) => ({ key: item[prop], index: i }))
+    a.sort((first, other) => {
+      if (first.key == other.key)
+        return first.index - other.index
+      return first.key < other.key ? -1 : 1
+    })
+    this.sortOrder = a.map(item => item.index)
   }
 }
